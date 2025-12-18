23f644cc78ffd9cea7bc283c485941b44226bbe5
Mayank Kumar <mk4664348@gmail.com>
2025-12-18 23:21:31 +0530

    Convex + clerk setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider, } from '@clerk/nextjs'
+import ConvexClerkProvider from "@/lib/ConvexClerkProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ConvexClerkProvider>
 
       <html lang="en">
         <body
@@ -33,6 +33,6 @@ export default function RootLayout({
           {children}
         </body>
       </html>
-    </ClerkProvider>
+    </ConvexClerkProvider>
   );
 }
---
+++ src/lib/ConvexClerkProvider.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { ConvexReactClient } from 'convex/react'
+import { ConvexProviderWithClerk } from 'convex/react-clerk'
+import { useAuth, ClerkProvider } from '@clerk/nextjs'
+import { ReactNode } from 'react';
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+export default function ConvexClerkProvider({ children }: { children: ReactNode }) {
+    return (
+        <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}>
+            <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+                {children}
+            </ConvexProviderWithClerk>
+        </ClerkProvider>
+    )
+}
